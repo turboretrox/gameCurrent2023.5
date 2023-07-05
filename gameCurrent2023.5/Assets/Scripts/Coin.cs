1d0e441002794bc43388bc01c61340eab5c6a639
turboretrox <mohamedsayedahmed001@gmail.com>
2023-07-05 03:23:22 +0200

    Gems&Coin

--- gameCurrent2023.5/Assets/Scripts/Coin.cs
+++ gameCurrent2023.5/Assets/Scripts/Coin.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
 
     int ScoreNum = 0;
+    public Text scoreText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Destroy(gameObject);
             ScoreNum += 10;
-            Debug.Log("Coin: " + ScoreNum);
+            scoreText.text = "Score: " + ScoreNum.ToString();
         }
     }
 }
--- gameCurrent2023.5/Assets/Scripts/Gem.cs
+++ gameCurrent2023.5/Assets/Scripts/Gem.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gem : MonoBehaviour
 {
     public static int GemsNum = 0;
+    public Text GemText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             GemsNum++;
-            Debug.Log("Gems :" + GemsNum);
+            GemText.text = GemsNum.ToString();
             Destroy(gameObject);
         }
     }
